01dabcb1abd3279ae61093d09269099c9e5ca5ba
Dalilah Earley <lila@DalilahsMacBook.hsd1.il.comcast.net>
2019-10-18 22:28:08 -0500

    added trending to menu

--- WEB-INF/classes/Trending.java
+++ WEB-INF/classes/Trending.java
@@ -7,95 +7,86 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 @WebServlet("/Trending")
 
 public class Trending extends HttpServlet {
 
+	ArrayList <Mostsold> mostsold = new ArrayList <Mostsold> ();
+    ArrayList <Mostsoldzip> mostsoldzip = new ArrayList <Mostsoldzip> ();
+	ArrayList <Bestrating> bestrated = new ArrayList <Bestrating> ();
+
 	protected void doGet(HttpServletRequest request,
 		HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter pw = response.getWriter();
+		mostsold = MongoDBDataStoreUtilities.mostsoldProducts();
+	
+		mostsoldzip = MongoDBDataStoreUtilities.mostsoldZip();
+		bestrated      = MongoDBDataStoreUtilities.topProducts();
 
-		/* Checks the Consoles type whether it is microsft or sony or nintendo then add products to hashmap*/
+		
 
 		String name = "Trending";
-		String CategoryName = request.getParameter("maker");
-		HashMap<String, Tv> hm = new HashMap<String, Tv>();
-		if(CategoryName==null)
-		{
-			hm.putAll(SaxParser4BestDealXMLdataStore1.tvs);
-		}
-		else
-		{
-			if(CategoryName.equals("samsung"))
-			{
-				for(Map.Entry<String,Tv> entry : SaxParser4BestDealXMLdataStore1.tvs.entrySet())
-				{
-				  if(entry.getValue().getRetailer().equals("Samsung")) 
-				  {
-					 hm.put(entry.getValue().getId(),entry.getValue());
-				  }
-				}
-			}
-			else if(CategoryName.equals("lcl"))
-			{
-				for(Map.Entry<String,Tv> entry : SaxParser4BestDealXMLdataStore1.tvs.entrySet())
-				{
-				  if(entry.getValue().getRetailer().equals("LCL"))
-				  {
-					 hm.put(entry.getValue().getId(),entry.getValue());
-				  }
-				}
-			}
-			else if(CategoryName.equals("sony"))
-			{
-				for(Map.Entry<String,Tv> entry : SaxParser4BestDealXMLdataStore1.tvs.entrySet())
-				{ 
-			      if(entry.getValue().getRetailer().equals("Sony"))
-				  {
-					 hm.put(entry.getValue().getId(),entry.getValue());
-				  }
-				}
-			}
-		}
 		
+
 		Utilities utility = new Utilities(request, pw);
 		utility.printHtml("Header.html");
 		utility.printHtml("Sidebar.html");
 		pw.print("<div id='content'><div class='post'><h2 class='title meta'>");
-		pw.print("<a style='font-size: 24px;'>"+name+" Products</a>");
+		pw.print("<a style='font-size: 24px;'>Best Products</a>");
 		pw.print("</h2><div class='entry'><table id='bestseller'>");
-		int i = 1; int size= hm.size();
-		for(Map.Entry<String, Tv> entry : hm.entrySet()){
-			Tv tv = entry.getValue();
-			if(i%3==1) pw.print("<tr>");
-			pw.print("<td><div id='shop_item'>");
-			pw.print("<h3>"+tv.getName()+"</h3>");
-			pw.print("<strong>$"+tv.getPrice()+"</strong><ul>");
-			pw.print("<li id='item'><img src='images/tv_images/"+tv.getImage()+"' alt='' /></li>");
-			pw.print("<li><form method='post' action='Cart'>" +
-					"<input type='hidden' name='name' value='"+entry.getKey()+"'>"+
-					"<input type='hidden' name='type' value='tvs'>"+
-					"<input type='hidden' name='maker' value='"+tv.getRetailer()+"'>"+
-					"<input type='hidden' name='access' value=''>"+
-					"<input type='submit' class='btnbuy' value='Buy Now'></form></li>");
-			pw.print("<li><form method='post' action='WriteReview'>"+"<input type='hidden' name='name' value='"+entry.getKey()+"'>"+
-					"<input type='hidden' name='type' value='tvs'>"+
-					"<input type='hidden' name='maker' value='"+tv.getRetailer()+"'>"+
-					"<input type='hidden' name='access' value=''>"+
-				    "<input type='submit' value='WriteReview' class='btnreview'></form></li>");
-			pw.print("<li><form method='post' action='ViewReview'>"+"<input type='hidden' name='name' value='"+entry.getKey()+"'>"+
-					"<input type='hidden' name='type' value='tvs'>"+
-					"<input type='hidden' name='maker' value='"+tv.getRetailer()+"'>"+
-					"<input type='hidden' name='access' value=''>"+
-				    "<input type='submit' value='ViewReview' class='btnreview'></form></li>");
-			pw.print("</ul></div></td>");
-			
-			if(i%3==0 || i == size) pw.print("</tr>");
-			i++;
-		}		
+		Iterator itr2 = bestrated.iterator();
+        while(itr2.hasNext()) {
+         Bestrating best = (Bestrating)itr2.next();
+ 		pw.print("<tr>");
+		pw.print("<td>");
+		pw.print(best.getProductname());
+		pw.print("</td>");
+		pw.print("<td>");
+		pw.print(best.getRating());
+		pw.print("</td>");
+		pw.print("</tr>");
+        }
 		pw.print("</table></div></div></div>");	
+		
+		pw.print("<div id='content'><div class='post'><h2 class='title meta'>");
+		pw.print("<a style='font-size: 24px;'>Most Sold Products by Zipcode</a>");
+		pw.print("</h2><div class='entry'><table id='bestseller'>");
+		Iterator itr1 = mostsoldzip.iterator();
+         while(itr1.hasNext()) {
+         Mostsoldzip mostzip = (Mostsoldzip)itr1.next();
+ 		pw.print("<tr>");
+		pw.println("<td border: 1px >");
+		
+		pw.println(mostzip.getZipcode());
+		pw.println("</td>");
+		pw.println("<td border: 1px >");
+		pw.println(mostzip.getCount());
+		pw.println("</td>");
+		pw.println("</tr>");
+        }
+		pw.print("</table></div></div></div>");	
+		
+		pw.print("<div id='content'><div class='post'><h2 class='title meta'>");
+		pw.print("<a style='font-size: 24px;'>Most Sold Products</a>");
+		pw.print("</h2><div class='entry'><table id='bestseller'>");
+		
+         Iterator itr = mostsold.iterator();
+        while(itr.hasNext()) {
+         Mostsold most = (Mostsold)itr.next();
+ 		pw.println("<tr>");
+		pw.println("<td border: 1px >");
+		pw.println(most.getProductname());
+		pw.println("</td>");
+		pw.println("<td border: 1px >");
+		pw.println(most.getCount());
+		pw.println("</td>");
+		pw.println("</tr>");
+        }
+		pw.print("</table></div></div></div>");
+		
 		utility.printHtml("Footer.html");
 	}
 
